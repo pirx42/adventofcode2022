82c8980d7210ac630154e9b412b44675d3828112
pirx <andreas.rose@framefield.com>
2022-12-02 14:49:04 +0100

    day 2 part 1

--- main.js
+++ main.js
@@ -1,41 +1,33 @@
+// A / X for Rock, B / Y for Paper, and C / Z for Scissors
+// A / X > C / Z
+// C / Z > B / Y
+// B / Y > A / X
 
 var fs = require('fs');
 var path = require('path');
-var filePath = './inputDay1.txt';
+var filePath = './inputDay2.txt';
 
 const caloriesSums = new Array();
 
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 
-let currentCalories = 0;
+let chosePoints = { 'A': 1, 'X': 1, 'B': 2, 'Y': 2, 'C': 3, 'Z': 3 };
+let matchPoints = {
+    'AX': 3, 'AY': 6, 'AZ': 0,
+    'BX': 0, 'BY': 3, 'BZ': 6,
+    'CX': 6, 'CY': 0, 'CZ': 3,
+};
+
+let totalPoints = 0;
 buffer.toString().split('\n').forEach((element, index) => {
-    if (element.length == 0) {
-        caloriesSums.push(currentCalories);
-        currentCalories = 0;
-    }
-    else {
-        currentCalories += parseInt(element);
+    if (element.length > 0) {
+        let elements = element.split(' ');
+        let first = elements[0];
+        let second = elements[1];
+        let currentPoints = chosePoints[second] + matchPoints[first + second];
+        totalPoints += currentPoints;
     }
 });
 
-caloriesSums.sort((lhs, rhs) => parseInt(rhs) - parseInt(lhs));
-
-console.log(caloriesSums[0] + caloriesSums[1] + caloriesSums[2]);
-
-
-
-// const intputUrl = 'https://adventofcode.com/2022/day/1/input';
-// const https = require('https')
-// https.get(intputUrl, res => {
-//     let data = '';
-//     res.on('data', chunk => {
-//         data += chunk;
-//     });
-//     res.on('end', () => {
-//         // data = JSON.parse(data);
-//         console.log(data);
-//     })
-// }).on('error', err => {
-//     console.log(err.message);
-// }).end()
+console.log(totalPoints);
 
