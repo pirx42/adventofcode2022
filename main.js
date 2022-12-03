d6b4ed38f1824981a9b63814b3555ffc9fbabb0c
pirx <andreas.rose@framefield.com>
2022-12-03 10:38:15 +0100

    day 3 part 1

--- main.js
+++ main.js
@@ -5,33 +5,35 @@
 
 var fs = require('fs');
 var path = require('path');
-var filePath = './inputDay2.txt';
+var filePath = './inputDay3.txt';
 
 const caloriesSums = new Array();
 
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 
-let chosePoints = { 'A': 1, 'X': 1, 'B': 2, 'Y': 2, 'C': 3, 'Z': 3 };
-let choseShape = {
-    'AX': 'Z', 'AY': 'X', 'AZ': 'Y',
-    'BX': 'X', 'BY': 'Y', 'BZ': 'Z',
-    'CX': 'Y', 'CY': 'Z', 'CZ': 'X',
-};
-let matchPoints = {
-    'AX': 3, 'AY': 6, 'AZ': 0,
-    'BX': 0, 'BY': 3, 'BZ': 6,
-    'CX': 6, 'CY': 0, 'CZ': 3,
-};
+let priority = {};
+let p = 1;
+for (let i = 97; i <= 122; ++i, ++p) {
+    priority[String.fromCharCode(i)] = p;
+}
+for (let i = 65; i <= 90; ++i, ++p) {
+    priority[String.fromCharCode(i)] = p;
+}
 
 let totalPoints = 0;
 buffer.toString().split('\n').forEach((element, index) => {
     if (element.length > 0) {
-        let elements = element.split(' ');
-        let first = elements[0];
-        let second = elements[1];
-        let myshape = choseShape[first + second];
-        let currentPoints = chosePoints[myshape] + matchPoints[first + myshape];
-        totalPoints += currentPoints;
+        let first = element.slice(0, element.length / 2);
+        let second = element.slice(element.length / 2, element.length);
+        let duplicate;
+        [...first].forEach((fc) => {
+            [...second].forEach((sc) => {
+                if (fc == sc)
+                    duplicate = fc;
+            });
+        });
+
+        totalPoints += priority[duplicate];
     }
 });
 
