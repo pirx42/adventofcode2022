906d3383bcd01be9fe3a4151801e71639b69f7d9
pirx <andreas.rose@framefield.com>
2022-12-03 12:43:37 +0100

    day 3 part 2

--- main.js
+++ main.js
@@ -9,8 +9,6 @@ var filePath = './inputDay3.txt';
 
 const caloriesSums = new Array();
 
-let buffer = fs.readFileSync(path.join(__dirname, filePath));
-
 let priority = {};
 let p = 1;
 for (let i = 97; i <= 122; ++i, ++p) {
@@ -20,22 +18,28 @@ for (let i = 65; i <= 90; ++i, ++p) {
     priority[String.fromCharCode(i)] = p;
 }
 
+let buffer = fs.readFileSync(path.join(__dirname, filePath));
+let lines = buffer.toString().split('\n');
+
 let totalPoints = 0;
-buffer.toString().split('\n').forEach((element, index) => {
-    if (element.length > 0) {
-        let first = element.slice(0, element.length / 2);
-        let second = element.slice(element.length / 2, element.length);
-        let duplicate;
+for (let i = 0; i < lines.length; ++i) {
+    if (lines[i].length > 0 && (i % 3) == 0) {
+        let first = lines[i];
+        let second = lines[i + 1];
+        let third = lines[i + 2];
+        let id;
         [...first].forEach((fc) => {
             [...second].forEach((sc) => {
-                if (fc == sc)
-                    duplicate = fc;
+                [...third].forEach((tc) => {
+                    if (fc == sc && fc == tc)
+                        id = fc;
+                });
             });
         });
 
-        totalPoints += priority[duplicate];
+        totalPoints += priority[id];
     }
-});
+};
 
 console.log(totalPoints);
 
