223e7beee4e9dd5a541bf3b98eed2df94d6aef06
pirx <andreas.rose@framefield.com>
2022-12-06 08:51:33 +0100

    day 6 part 2

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-const { create } = require('domain');
 var fs = require('fs');
 var path = require('path');
 var filePath = './inputDay6.txt';
@@ -6,17 +5,13 @@ var filePath = './inputDay6.txt';
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 let input = buffer.toString();
 
-let lastCharacters = new Array(4).fill(' ');
+const lengthOfMarker = 14;
+let lastCharacters = new Array(lengthOfMarker).fill(' ');
 let currentIndex = 0;
 [...input].every((c) => {
     lastCharacters[currentIndex++ % lastCharacters.length] = c;
-    let characterSet = new Set();
-    [...lastCharacters].forEach((e) => characterSet.add(e));
-    if (currentIndex > 3 && characterSet.size == lastCharacters.length)
-        return false;
-    return true;
+    return currentIndex <= lengthOfMarker - 1 || new Set(lastCharacters).size != lastCharacters.length;
 });
-
 
 console.log(currentIndex);
 
