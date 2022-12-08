f16aa0892946afe76aadc3fd052b321d6f564359
pirx <andreas.rose@framefield.com>
2022-12-08 08:57:02 +0100

    day 8 part 1

--- main.js
+++ main.js
@@ -1,130 +1,75 @@
 var fs = require('fs');
 var path = require('path');
-var filePath = './inputDay7.txt';
+var filePath = './inputDay8.txt';
 
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 let input = buffer.toString();
 
+// let testInput = '30373\n\
+// 25512\n\
+// 65332\n\
+// 33549\n\
+// 35390\n';
 
-// let testInput = '$ cd /\n\
-// $ ls\n\
-// dir a\n\
-// 14848514 b.txt\n\
-// 8504156 c.dat\n\
-// dir d\n\
-// $ cd a\n\
-// $ ls\n\
-// dir e\n\
-// 29116 f\n\
-// 2557 g\n\
-// 62596 h.lst\n\
-// $ cd e\n\
-// $ ls\n\
-// 584 i\n\
-// $ cd ..\n\
-// $ cd ..\n\
-// $ cd d\n\
-// $ ls\n\
-// 4060174 j\n\
-// 8033020 d.log\n\
-// 5626152 d.ext\n\
-// 7214296 k\n';
-
-//parse and build folder tree
-let root = { 'parent': null, 'name': '/', 'content': [] };
-
-let currentFolder = root;
+let treeGrid = [];
 input.split('\n').forEach((line, index) => {
     if (line.length > 0) {
-        // const reCmdLS = /\$ ls/g;
-        // let match = reCmdLS.exec(line);
-        // if (match) {
-
-        // }
-
-        const reCmdCDX = /\$ cd (\S+)/g;
-        match = reCmdCDX.exec(line);
-        if (match) {
-            if (match[1] == '/')
-                currentFolder = root;
-            else if (match[1] == '..')
-                currentFolder = folderUp(currentFolder);
-            else
-                currentFolder = folderDown(currentFolder, match[1]);
-        }
-
-        const reDIRX = /dir (\S+)/g;
-        match = reDIRX.exec(line);
-        if (match) {
-            addSubFolder(currentFolder, match[1]);
-        }
-
-        const reFILEX = /(\d+) (\S+)/g;
-        match = reFILEX.exec(line);
-        if (match) {
-            addFile(currentFolder, match[2], parseInt(match[1]));
-        }
+        treeGrid.push([...line]);
     }
 });
 
+let gridWidth = treeGrid[0].length;
+let gridHeight = treeGrid.length;
 
-updateContentsSize(root);
-
-let orderedFoldersBySize = new Array();
-flattenFolder(root);
-orderedFoldersBySize.sort((a, b) => a.size - b.size);
-
-let requiredSize = 30000000;
-let unusedSpace = 70000000 - root.size;
-let minimumSpaceToFree = requiredSize - unusedSpace;
-
-orderedFoldersBySize.every((folder) => {
-    if (folder.size >= minimumSpaceToFree) {
-        console.log(folder.size);
-        return false;
+let numVisibleTrees = 0;
+for (let row = 1; row < gridHeight - 1; ++row) {
+    for (let column = 1; column < gridWidth - 1; ++column) {
+        if (isTreeVisible(column, row))
+            ++numVisibleTrees;
     }
-    return true;;
-})
-
-
-function flattenFolder(folder) {
-    folder.content.forEach((element) => {
-        if (element.hasOwnProperty('parent'))
-            flattenFolder(element);
-    });
-
-    orderedFoldersBySize.push(folder);
 }
 
+console.log(gridWidth * 2 + (gridHeight - 2) * 2 + numVisibleTrees);
 
-function updateContentsSize(folder) {
-    folder.content.forEach((element) => {
-        if (element.hasOwnProperty('parent'))
-            updateContentsSize(element);
-    });
 
-    let folderSize = 0;
-    folder.content.forEach((element) => { folderSize += element.size; });
-    folder.size = folderSize;
+
+function isTreeVisible(column, row) {
+    let visibleToLeft = true;
+    for (let c = 0; c < column; ++c) {
+        if (treeGrid[row][c] >= treeGrid[row][column]) {
+            visibleToLeft = false;
+            break;
+        }
+    }
+    if (visibleToLeft)
+        return true;
+
+    let visibleToRight = true;
+    for (let c = column + 1; c < gridHeight; ++c) {
+        if (treeGrid[row][c] >= treeGrid[row][column]) {
+            visibleToRight = false;
+            break;
+        }
+    }
+    if (visibleToRight)
+        return true;
+
+    let visibleToTop = true;
+    for (let r = 0; r < row; ++r) {
+        if (treeGrid[r][column] >= treeGrid[row][column]) {
+            visibleToTop = false;
+            break;
+        }
+    }
+    if (visibleToTop)
+        return true;
+
+    let visibleToBottom = true;
+    for (let r = row + 1; r < gridHeight; ++r) {
+        if (treeGrid[r][column] >= treeGrid[row][column]) {
+            visibleToBottom = false;
+            break;
+        }
+    }
+    return visibleToBottom;
 }
-
-
-function folderUp(folder) {
-    return folder.parent;
-}
-
-function folderDown(folder, name) {
-    return folder.content.find((element) => { return element.name == name; });
-}
-
-function addSubFolder(folder, name) {
-    let subFolder = { 'parent': folder, 'name': name, 'size': 0, 'content': [] };
-    folder.content.push(subFolder);
-    return subFolder;
-}
-
-function addFile(folder, name, size) {
-    folder.content.push({ 'name': name, 'size': size });
-}
-
-
