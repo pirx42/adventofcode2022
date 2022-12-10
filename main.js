087a33be97f93480606a3b2a9623776c92d11c06
pirx <andreas.rose@framefield.com>
2022-12-10 07:20:31 +0100

    day 10 part 1

--- main.js
+++ main.js
@@ -1,82 +1,31 @@
 var fs = require('fs');
 var path = require('path');
-var filePath = './inputDay9.txt';
+var filePath = './inputDay10.txt';
 
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 let input = buffer.toString();
 
-// let testInput = 'R 4\n\
-// U 4\n\
-// L 3\n\
-// D 1\n\
-// R 4\n\
-// D 1\n\
-// L 5\n\
-// R 2\n';
-
-let rolledOutMotions = [];
+let rolledInstructionsPerCycle = [];
 input.split('\n').forEach((line, index) => {
     if (line.length > 0) {
-        const reCmdCDX = /(\S) (\d+)/g;
-        match = reCmdCDX.exec(line);
-        if (match[1] == 'U') {
-            for (let i = 0; i < parseInt(match[2]); ++i)
-                rolledOutMotions.push([0, -1]);
+        const reCmdAddX = /addx (\S+)/g;
+        match = reCmdAddX.exec(line);
+        if (match) {
+            rolledInstructionsPerCycle.push(0);
+            rolledInstructionsPerCycle.push(parseInt(match[1]));
         }
-        else if (match[1] == 'L') {
-            for (let i = 0; i < parseInt(match[2]); ++i)
-                rolledOutMotions.push([-1, 0]);
-        }
-        else if (match[1] == 'D') {
-            for (let i = 0; i < parseInt(match[2]); ++i)
-                rolledOutMotions.push([0, 1]);
-        }
-        else if (match[1] == 'R') {
-            for (let i = 0; i < parseInt(match[2]); ++i)
-                rolledOutMotions.push([1, 0]);
+        else { //noop
+            rolledInstructionsPerCycle.push(0);
         }
     }
 });
 
-let headPosition = [0, 0];
-let tailPositions = [];
-for (let i = 1; i < 10; ++i)
-    tailPositions.push([0, 0]);
-
-let tailVisitedMap = new Set();
-
-rolledOutMotions.forEach((element) => {
-    headPosition[0] += element[0];
-    headPosition[1] += element[1];
-
-    updateTail(headPosition, tailPositions[0]);
-    updateTail(tailPositions[0], tailPositions[1]);
-    updateTail(tailPositions[1], tailPositions[2]);
-    updateTail(tailPositions[2], tailPositions[3]);
-    updateTail(tailPositions[3], tailPositions[4]);
-    updateTail(tailPositions[4], tailPositions[5]);
-    updateTail(tailPositions[5], tailPositions[6]);
-    updateTail(tailPositions[6], tailPositions[7]);
-    updateTail(tailPositions[7], tailPositions[8]);
-    tailVisitedMap.add(tailPositions[8][0].toString() + '|' + tailPositions[8][1].toString());
+let registerValue = 1;
+let summedSignalStrength = 0;
+rolledInstructionsPerCycle.forEach((ipc, index) => {
+    if (index == 20 || index == 60 || index == 100 || index == 140 || index == 180 || index == 220)
+        summedSignalStrength += registerValue * index;
+    registerValue += ipc;
 });
 
-console.log(tailVisitedMap.size);
-
-
-function updateTail(headPosition, tailPosition) {
-    let headTailOffset = [headPosition[0] - tailPosition[0], headPosition[1] - tailPosition[1]];
-    if (Math.abs(headTailOffset[0]) <= 1 && Math.abs(headTailOffset[1]) <= 1)
-        return;
-    let tailMotion = headTailOffset;
-    if (tailMotion[0] > 1)
-        tailMotion[0] = 1;
-    else if (tailMotion[0] < -1)
-        tailMotion[0] = -1;
-    if (tailMotion[1] > 1)
-        tailMotion[1] = 1;
-    else if (tailMotion[1] < -1)
-        tailMotion[1] = -1;
-    tailPosition[0] += tailMotion[0];
-    tailPosition[1] += tailMotion[1];
-}
+console.log(summedSignalStrength);
