563201b1ba8e38f2be941917be3287402b251896
pirx <andreas.rose@framefield.com>
2022-12-13 23:20:19 +0100

    day 13 part 1

--- main.js
+++ main.js
@@ -1,98 +1,49 @@
 var fs = require('fs');
 var path = require('path');
-var filePath = './inputDay12.txt';
+var filePath = './inputDay13.txt';
 
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 let input = buffer.toString();
 
-let heightMap = [];
-input.split('\n').forEach((line, index) => {
-    if (line.length > 0) {
-        heightMap.push([...line]);
+let summedIndices = 0;
+let lines = input.split('\n');
+for (let i = 0; i < lines.length; i += 3) {
+    if (lines[0].length == 0)
+        break;
+
+    let first = JSON.parse(lines[i]);
+    let second = JSON.parse(lines[i + 1]);
+    if (isRightOrder(first, second) <= 0) {
+        summedIndices += Math.trunc(i / 3 + 1);
     }
-});
+}
 
-let start = [0, 0];
-let destination = [0, 0];
-let distanceMap = new Array(heightMap.length);
+console.log(summedIndices);
 
-for (let y = 0; y < heightMap.length; ++y) {
-    for (let x = 0; x < heightMap[0].length; ++x) {
-        if (heightMap[y][x] == 'S') {
-            start = [x, y];
-            heightMap[y][x] = 'a';
+function isRightOrder(lhs, rhs) {
+    if (Number.isInteger(lhs) && Number.isInteger(rhs)) {
+        return lhs - rhs;
+    }
+    else if (Array.isArray(lhs) && Array.isArray(rhs)) {
+        for (let i = 0; i < Math.max(lhs.length, rhs.length); ++i) {
+            if (i >= lhs.length && i < rhs.length)
+                return -1;
+            if (i < lhs.length && i >= rhs.length)
+                return 1;
+
+            let result = isRightOrder(lhs[i], rhs[i]);
+            if (result < 0)
+                return -1;
+            else if (result > 0)
+                return 1;
         }
-        else if (heightMap[y][x] == 'E') {
-            destination = [x, y];
-            heightMap[y][x] = 'z';
-        }
-        heightMap[y][x] = heightMap[y][x].charCodeAt() - 97;
-    }
-    distanceMap[y] = new Array(heightMap[0].length).fill(99999);
-}
-
-
-const neighbors = [[1, 0], [0, 1], [-1, 0], [0, -1]];
-
-let foundDistances = [];
-
-for (let y = 0; y < heightMap.length; ++y) {
-    for (let x = 0; x < heightMap[0].length; ++x) {
-        let pos = [x, y];
-        if (levelAt(pos) == 0) {
-            initializeDistanceMap();
-            updateDistancesOfNeighbors(pos, 0);
-            foundDistances.push(distanceAt(destination));
-        }
-    }
-}
-
-foundDistances.sort();
-console.log(foundDistances[0]);
-
-
-function updateDistancesOfNeighbors(current, distance) {
-    distanceMap[current[1]][current[0]] = distance;
-
-    if (equal(current, destination))
-        return;
-
-    for (let i = 0; i < neighbors.length; ++i) {
-        let next = add(current, neighbors[i]);
-        if (levelAt(next) > levelAt(current) + 1)
-            continue;
-        if (distanceAt(next) <= distance + 1)
-            continue;
-        updateDistancesOfNeighbors(next, distance + 1);
-    }
-}
-
-function initializeDistanceMap() {
-    for (let y = 0; y < distanceMap.length; ++y) {
-        for (let x = 0; x < distanceMap[0].length; ++x) {
-            distanceMap[y][x] = 99999;
-        }
-    }
-}
-
-function levelAt(pos) {
-    if (pos[0] < 0 || pos[0] >= heightMap[0].length ||
-        pos[1] < 0 || pos[1] >= heightMap.length)
-        return -1;
-    return heightMap[pos[1]][pos[0]];
-}
-
-function distanceAt(pos) {
-    if (pos[0] < 0 || pos[0] >= distanceMap[0].length ||
-        pos[1] < 0 || pos[1] >= distanceMap.length)
         return 0;
-    return distanceMap[pos[1]][pos[0]];
-}
-
-function add(p1, p2) {
-    return [p1[0] + p2[0], p1[1] + p2[1]];
-}
-
-function equal(p1, p2) {
-    return p1[0] == p2[0] && p1[1] == p2[1];
+    }
+    else if (Number.isInteger(lhs)) {
+        return isRightOrder([lhs], rhs);
+    }
+    else if (Number.isInteger(rhs)) {
+        return isRightOrder(lhs, [rhs]);
+    }
+    return 0;
 }
