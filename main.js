0f98ea409341f94f470173126a5aeae83948dc52
pirx <andreas.rose@framefield.com>
2023-01-05 20:50:28 +0100

    day 21 part 2

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var filePath = './inputDay21.txt';
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 let input = buffer.toString();
 
+const humanId = 'humn';
 let monkeys = {};
 
 let lines = input.split('\n');
@@ -16,12 +17,56 @@ lines.forEach((line) => {
         let matchNumber = reNumber.exec(line);
         if (matchOp) {
             let monkey = { id: matchOp[1], inputA: matchOp[2], inputB: matchOp[4] };
-            eval('monkey.evalutate = function () { return this.inputA.evalutate() ' + matchOp[3] + ' this.inputB.evalutate() }');
+            eval('monkey.evaluate = function () { return this.inputA.evaluate() ' + matchOp[3] + ' this.inputB.evaluate() }');
+
+            switch (matchOp[3]) {
+                case '+':
+                    monkey.set = function (val) {
+                        let humanInSubtreeA = findMonkey(this.inputA, humanId);
+                        if (humanInSubtreeA)
+                            this.inputA.set(val - this.inputB.evaluate());
+                        else
+                            this.inputB.set(val - this.inputA.evaluate());
+                    }
+                    break;
+                case '-':
+                    monkey.set = function (val) {
+                        let humanInSubtreeA = findMonkey(this.inputA, humanId);
+                        if (humanInSubtreeA)
+                            this.inputA.set(val + this.inputB.evaluate());
+                        else
+                            this.inputB.set(this.inputA.evaluate() - val);
+                    }
+                    break;
+                case '/':
+                    monkey.set = function (val) {
+                        let humanInSubtreeA = findMonkey(this.inputA, humanId);
+                        if (humanInSubtreeA)
+                            this.inputA.set(val * this.inputB.evaluate());
+                        else
+                            this.inputB.set(this.inputA.evaluate() / val);
+                    }
+                    break;
+                case '*':
+                    monkey.set = function (val) {
+                        let humanInSubtreeA = findMonkey(this.inputA, humanId);
+                        if (humanInSubtreeA)
+                            this.inputA.set(val / this.inputB.evaluate());
+                        else
+                            this.inputB.set(val / this.inputA.evaluate());
+                    }
+                    break;
+            };
+
             monkeys[matchOp[1]] = monkey;
         }
         else if (matchNumber) {
             let monkey = { id: matchNumber[1] };
-            eval('monkey.evalutate = function () { return ' + matchNumber[2] + ' }');
+            eval('monkey.evaluate = function () { return ' + matchNumber[2] + ' }');
+            if (matchNumber[1] == humanId) {
+                monkey.value = -1;
+                monkey.set = function (val) { this.value = val; };
+            }
             monkeys[matchNumber[1]] = monkey;
         }
     }
@@ -37,4 +82,33 @@ Object.keys(monkeys).forEach((key) => {
 });
 
 
-console.log(monkeys['root'].evalutate());
+let rootInputA = monkeys['root'].inputA;
+let rootInputB = monkeys['root'].inputB;
+
+let humanInSubtreeA = findMonkey(rootInputA, humanId);
+let humanInSubtreeB = findMonkey(rootInputB, humanId);
+
+if (humanInSubtreeA) {
+    rootInputA.set(rootInputB.evaluate());
+    console.log(humanInSubtreeA.value);
+}
+else {
+    rootInputB.set(rootInputA.evaluate());
+    console.log(humanInSubtreeB.value);
+}
+
+//helper
+function findMonkey(monkey, id) {
+    if (monkey == null)
+        return null;
+    if (monkey.id == id)
+        return monkey;
+    let foundMonkeyInSubTreeA = findMonkey(monkey.inputA, id);
+    if (foundMonkeyInSubTreeA)
+        return foundMonkeyInSubTreeA;
+    let foundMonkeyInSubTreeB = findMonkey(monkey.inputB, id);
+    if (foundMonkeyInSubTreeB)
+        return foundMonkeyInSubTreeB;
+    return null;
+}
+
