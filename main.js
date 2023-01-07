296d29bf70e399f91faa015e5b7a5d005efabd02
pirx <andreas.rose@framefield.com>
2023-01-08 00:34:04 +0100

    day 18 part 1

--- main.js
+++ main.js
@@ -1,131 +1,95 @@
 var fs = require('fs');
 var path = require('path');
-var filePath = './inputDay20.txt';
-
-
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-        this.previous = null;
-    }
-}
-
-//double linked cyclic list
-class CyclicList {
-    constructor() {
-        this.head = null;
-    }
-
-    firstNode() {
-        return this.head;
-    }
-
-    append(data) { //add at end mean before head
-        if (this.head == null) {
-            this.head = new Node(data);
-            this.head.next = this.head;
-            this.head.previous = this.head;
-            return this.head;
-        }
-        else {
-            let n = new Node(data);
-            n.next = this.head;
-            let nodeBeforeHead = this.head.previous;
-            nodeBeforeHead.next = n;
-            n.previous = nodeBeforeHead
-            this.head.previous = n;
-            n.next = this.head;
-            return n;
-        }
-    }
-
-    move(node, distance) {
-        if (distance == 0)
-            return;
-        let destination = node.previous;
-        this.extract(node);
-        if (distance > 0) {
-            while (distance--)
-                destination = destination.next;
-        } else {
-            while (distance++)
-                destination = destination.previous;
-        }
-        this.insert(node, destination);
-    }
-
-    extract(node) {
-        let before = node.previous;
-        let after = node.next;
-        before.next = after;
-        after.previous = before;
-        if (this.head == node)
-            this.head = after;
-    }
-
-    insert(node, destination) {
-        // insert after destination
-        let newBefore = destination;
-        let newAfter = destination.next;
-        newBefore.next = node;
-        node.previous = newBefore;
-        newAfter.previous = node;
-        node.next = newAfter;
-    }
-
-    getNthAfter(node, n) {
-        if (n == 0)
-            return node;
-        let nthNode = node;
-        if (n > 0) {
-            while (n--)
-                nthNode = nthNode.next;
-        } else {
-            while (n++)
-                nthNode = nthNode.previous;
-        }
-        return nthNode;
-    }
-
-    toString() {
-        let s = '';
-        let current = this.head;
-        do {
-            s += current.data + ', ';
-            current = current.next;
-        } while (current != this.head)
-        return s;
-    }
-}
+var filePath = './inputDay18.txt';
 
 
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
 let input = buffer.toString();
 
-const key = 811589153;
-
-let elementsInInitialOrder = [];
-let elementListForMixing = new CyclicList();
-let nodeWithValue0 = null;
-
+let max = [0, 0, 0];
+let elements = [];
 let lines = input.split('\n');
 lines.forEach((line) => {
     if (line.length > 0) {
-        let addedNode = elementListForMixing.append(parseInt(line) * key);
-        elementsInInitialOrder.push(addedNode);
-        if (addedNode.data == 0)
-            nodeWithValue0 = addedNode;
+        const regex = /(\S+),(\S+),(\S+)/g;
+        let match = regex.exec(line);
+        let element = [parseInt(match[1]), parseInt(match[2]), parseInt(match[3])];
+        max[0] = Math.max(max[0], element[0]);
+        max[1] = Math.max(max[1], element[1]);
+        max[2] = Math.max(max[2], element[2]);
+        elements.push(element);
     }
 });
 
-for (let i = 0; i < 10; ++i) {
-    elementsInInitialOrder.forEach((nodeToMove) => {
-        elementListForMixing.move(nodeToMove, nodeToMove.data % (elementsInInitialOrder.length - 1));
-    });
+let size = add(max, [1, 1, 1]);
+let grid = new Array(size[2]).fill(0);
+for (let z = 0; z < size[2]; ++z) {
+    grid[z] = new Array(size[1]).fill(0);
+    for (let y = 0; y < size[1]; ++y) {
+        grid[z][y] = new Array(size[0]).fill(0);
+    }
 }
 
-let node1000 = elementListForMixing.getNthAfter(nodeWithValue0, 1000);
-let node2000 = elementListForMixing.getNthAfter(nodeWithValue0, 2000);
-let node3000 = elementListForMixing.getNthAfter(nodeWithValue0, 3000);
-console.log(node1000.data + node2000.data + node3000.data);
+elements.forEach((element) => {
+    setElementAt(element, 1);
+});
+
+let neighbors = [
+    [1, 0, 0],
+    [-1, 0, 0],
+    [0, 1, 0],
+    [0, -1, 0],
+    [0, 0, 1],
+    [0, 0, -1],
+];
+
+let numTotalOutsideFaces = 0;
+for (let z = 0; z < size[2]; ++z) {
+    for (let y = 0; y < size[1]; ++y) {
+        for (let x = 0; x < size[0]; ++x) {
+
+            let current = [x, y, z];
+            if (elementAt(current) > 0) {
+                let outsideFaces = 6;
+                neighbors.forEach((neighborOffset) => {
+                    let neighbor = add(current, neighborOffset);
+                    if (elementAt(neighbor) > 0)
+                        --outsideFaces;
+                });
+                numTotalOutsideFaces += outsideFaces;
+            }
+
+        }
+    }
+}
+
+console.log(numTotalOutsideFaces);
+
+//helper
+function elementAt(pos) {
+    if (pos[0] < 0 || pos[0] >= size[0] ||
+        pos[1] < 0 || pos[1] >= size[1] ||
+        pos[2] < 0 || pos[2] >= size[2])
+        return -1;
+    return grid[pos[2]][pos[1]][pos[0]];
+}
+
+function setElementAt(pos, element) {
+    if (pos[0] < 0 || pos[0] >= size[0] ||
+        pos[1] < 0 || pos[1] >= size[1] ||
+        pos[2] < 0 || pos[2] >= size[2])
+        return;
+    grid[pos[2]][pos[1]][pos[0]] = element;
+}
+
+function add(p1, p2) {
+    return [p1[0] + p2[0], p1[1] + p2[1], p1[2] + p2[2]];
+}
+
+function sub(p1, p2) {
+    return [p1[0] - p2[0], p1[1] - p2[1], p1[2] - p2[2]];
+}
+
+function mul(p, t) {
+    return [p[0] * t, p[1] * t, p[2] * t];
+}
