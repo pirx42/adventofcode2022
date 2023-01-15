63a8e9410482575e1622f1bd2aa27c2a578d9a52
pirx <andreas.rose@framefield.com>
2023-01-15 19:29:46 +0100

    day 19 part 1

--- main.js
+++ main.js
@@ -1,130 +1,176 @@
 var fs = require('fs');
 var path = require('path');
-var filePath = './inputDay17.txt';
+var filePath = './inputDay19.txt';
 
 let buffer = fs.readFileSync(path.join(__dirname, filePath));
+let input = buffer.toString();
 
-let commands = [...buffer.toString()];
-commands.pop();//remove line break
+const MAX_TIME = 24;
+const ORE_ROBOT_ID = 0;
+const CLAY_ROBOT_ID = 1;
+const OBSIDIAN_ROBOT_ID = 2;
+const GEODE_ROBOT_ID = 3;
 
-const OUT = -1;
-const FREE = 0;
-const ROCK = 1;
+let bluePrints = [];
+let states = { oreRobots: 1, clayRobots: 0, obsidianRobots: 0, geodeRobots: 0, collectedOre: 0, collectedClay: 0, collectedObsidian: 0, collectedGeode: 0 };
 
-let rockShape1 = { id: 1, height: 1, parts: [[0, 0], [1, 0], [2, 0], [3, 0]] };
-let rockShape2 = { id: 2, height: 3, parts: [[1, 0], [0, -1], [1, -1], [2, -1], [1, -2]] };
-let rockShape3 = { id: 3, height: 3, parts: [[0, 0], [1, 0], [2, 0], [2, -1], [2, -2]] };
-let rockShape4 = { id: 4, height: 4, parts: [[0, 0], [0, -1], [0, -2], [0, -3]] };
-let rockShape5 = { id: 5, height: 2, parts: [[0, 0], [1, 0], [0, -1], [1, -1]] };
+input.split('\n').forEach((line) => {
+    if (line.length > 0) {
+        const regex = /Blueprint (\S+): Each ore robot costs (\S+) ore. Each clay robot costs (\S+) ore. Each obsidian robot costs (\S+) ore and (\S+) clay. Each geode robot costs (\S+) ore and (\S+) obsidian./g;
+        let match = regex.exec(line);
 
-let rockShapes = [rockShape1, rockShape2, rockShape3, rockShape4, rockShape5];
-
-let currentMovingRockIdx = 0;
-let currentCommandIdx = 0;
-let currentMaximalRockHigh = 0;
-
-const MAP_WIDTH = 7;
-let simulationStateMap = [];
-
-//the target simulation count is too high for any calculation. there must be some kind of periodicity in the simulation.
-//this needs to be found.
-//after analyzing the simulated rocks visually we notice that after rock high of 136 the simulation repeats with periodic length of 2548 high.
-//those hights corresponds to 91 and 1690 fallen rocks. that means we only need to simulate the first 91 rocks plus the remainder of (1000000000000 - 91) / 1690 = 469 rocks.
-//so in total 560 rocks need to be simulated.
-//afterwards we can calculate the reached high by trunc((1000000000000 - 91) / 1690) * 2548 plus the simulated high.
-
-let rockFallsToSimulate = 91 + (1000000000000 - 91) % 1690;
-
-while (currentMovingRockIdx < rockFallsToSimulate) {
-
-    let rockShape = rockShapes[currentMovingRockIdx % rockShapes.length];
-    ++currentMovingRockIdx;
-    let currentPosition = [2, rockShape.height - 1];
-    let rowsToAdd = rockShape.height + 3 + currentMaximalRockHigh - simulationStateMap.length
-    if (rowsToAdd > 0) {
-        for (let i = 0; i < rowsToAdd; ++i)
-            simulationStateMap.unshift(new Array(MAP_WIDTH).fill(FREE));
+        let bluePrint = {
+            id: parseInt(match[1]),
+            oreCosts: [parseInt(match[2]), parseInt(match[3]), parseInt(match[4]), parseInt(match[6])],
+            clayCosts: [0, 0, parseInt(match[5]), 0],
+            obsidianCosts: [0, 0, 0, parseInt(match[7])]
+        };
+        bluePrints.push(bluePrint);
     }
-    else {
-        for (let i = 0; i < Math.abs(rowsToAdd); ++i)
-            simulationStateMap.shift();
-    }
+});
 
-    while (true) {
-        let command = commands[currentCommandIdx % commands.length];
-        ++currentCommandIdx;
-        let nextPosition = add(currentPosition, command == '<' ? [-1, 0] : [1, 0]);
+let results = [];
 
-        if (!hasCollision(rockShape, nextPosition)) {
-            currentPosition = nextPosition;
-        }
+let bestScore = 0;
+let timeFirstGeodeFound = 999;
+let bluePrint = bluePrints[0];
 
-        nextPosition = add(currentPosition, [0, 1]);
-        if (hasCollision(rockShape, nextPosition)) {
-            setShapeAt(rockShape, currentPosition);
-            let shapeRockHigh = simulationStateMap.length - currentPosition[1] + (rockShape.height - 1);
-            currentMaximalRockHigh = Math.max(currentMaximalRockHigh, shapeRockHigh);
-            break;
-        }
-        else {
-            currentPosition = nextPosition;
-        }
-    }
+for (let i = 0; i < bluePrints.length; ++i) {
+    bestScore = 0;
+    timeFirstGeodeFound = 999;
+    bluePrint = bluePrints[i];
+    doNextStep(structuredClone(states), 1);
+    results.push(bestScore);
+
+    console.log((i + 1) + ' ' + bestScore);
 }
 
-let totalReachedHigh = currentMaximalRockHigh + Math.trunc((1000000000000 - 91) / 1690) * 2548;
-console.log(totalReachedHigh);
+let sum = 0;
+for (let i = 0; i < bluePrints.length; ++i) {
+    sum += results[i] * (i + 1);
+}
+console.log(sum);
 
 
 //helper
-function hasCollision(shape, position) {
-    for (let i = 0; i < shape.parts.length; ++i) {
-        let partPos = add(position, shape.parts[i]);
-        if (elementAt(partPos) != FREE)
-            return true;
+function doNextStep(states, time) {
+
+    let newStates = structuredClone(states);
+    collectResources(newStates);
+
+    if (newStates.collectedGeode > bestScore)
+        bestScore = newStates.collectedGeode;
+
+    if (newStates.collectedGeode > 0 && time < timeFirstGeodeFound)
+        timeFirstGeodeFound = time;
+
+    if (newStates.collectedGeode == 0 && time > timeFirstGeodeFound)
+        return;
+
+    if (time == MAX_TIME)
+        return;
+
+    let nextStates = [];
+    {
+        let newStates = structuredClone(states);
+        let robotBuild = createRobot(time, GEODE_ROBOT_ID, bluePrint, newStates);
+        collectResources(newStates);
+        if (robotBuild) {
+            newStates.geodeRobots++;
+            nextStates.push(newStates);
+        }
+    }
+    {
+        let newStates = structuredClone(states);
+        let robotBuild = createRobot(time, OBSIDIAN_ROBOT_ID, bluePrint, newStates);
+        collectResources(newStates);
+        if (robotBuild) {
+            newStates.obsidianRobots++;
+            nextStates.push(newStates);
+        }
+    }
+    {
+        let newStates = structuredClone(states);
+        let robotBuild = createRobot(time, CLAY_ROBOT_ID, bluePrint, newStates);
+        collectResources(newStates);
+        if (robotBuild) {
+            newStates.clayRobots++;
+            nextStates.push(newStates);
+        }
+    }
+    {
+        let newStates = structuredClone(states);
+        let robotBuild = createRobot(time, ORE_ROBOT_ID, bluePrint, newStates);
+        collectResources(newStates);
+        if (robotBuild) {
+            newStates.oreRobots++;
+            nextStates.push(newStates);
+        }
+    }
+    {
+        let newStates = structuredClone(states);
+        collectResources(newStates);
+        nextStates.push(newStates);
+    }
+
+    nextStates.forEach((state) => {
+        doNextStep(state, time + 1);
+    });
+}
+
+function createRobot(time, id, bluePrint, states) {
+    let timeRemaining = MAX_TIME - time;
+    let requiredOre = bluePrint.oreCosts[id];
+    let requiredClay = bluePrint.clayCosts[id];
+    let requiredObsidian = bluePrint.obsidianCosts[id];
+    let numOreRobots = states.oreRobots;
+    let numClayRobots = states.clayRobots;
+    let numObsidianRobots = states.obsidianRobots;
+    switch (id) {
+        case ORE_ROBOT_ID: {
+            if (states.collectedOre >= requiredOre &&
+                numOreRobots < 4 &&
+                timeRemaining >= 14) {
+                states.collectedOre -= requiredOre;
+                return true;
+            }
+            break;
+        }
+        case CLAY_ROBOT_ID: {
+            if (states.collectedOre >= requiredOre &&
+                numClayRobots < bluePrint.clayCosts[OBSIDIAN_ROBOT_ID] - 1 &&
+                timeRemaining >= 6) {
+                states.collectedOre -= requiredOre;
+                return true;
+            }
+            break;
+        }
+        case OBSIDIAN_ROBOT_ID: {
+            if (states.collectedOre >= requiredOre && states.collectedClay >= requiredClay &&
+                timeRemaining >= 3 &&
+                numObsidianRobots < bluePrint.obsidianCosts[GEODE_ROBOT_ID]) {
+                states.collectedOre -= requiredOre;
+                states.collectedClay -= requiredClay;
+                return true;
+            }
+            break;
+        }
+        case GEODE_ROBOT_ID: {
+            if (states.collectedOre >= requiredOre && states.collectedObsidian >= requiredObsidian &&
+                timeRemaining >= 1) {
+                states.collectedOre -= requiredOre;
+                states.collectedObsidian -= requiredObsidian;
+                return true;
+            }
+            break;
+        }
     }
     return false;
 }
 
-function setShapeAt(shape, pos) {
-    shape.parts.forEach((part) => {
-        setElementAt(add(pos, part), ROCK);
-    });
-}
-
-function drawMap() {
-    for (let i = 0; i < simulationStateMap.length; ++i) {
-        console.log((simulationStateMap[i].map((e) => {
-            if (e == 0)
-                return '.'
-            else
-                return '#';
-        }).join('')));
-    }
-}
-
-function elementAt(pos) {
-    if (pos[0] < 0 || pos[0] >= simulationStateMap[0].length ||
-        pos[1] < 0 || pos[1] >= simulationStateMap.length)
-        return OUT;
-    return simulationStateMap[pos[1]][pos[0]];
-}
-
-function setElementAt(pos, element) {
-    if (pos[0] < 0 || pos[0] >= simulationStateMap[0].length ||
-        pos[1] < 0 || pos[1] >= simulationStateMap.length)
-        return;
-    simulationStateMap[pos[1]][pos[0]] = element;
-}
-
-function add(p1, p2) {
-    return [p1[0] + p2[0], p1[1] + p2[1]];
-}
-
-function sub(p1, p2) {
-    return [p1[0] - p2[0], p1[1] - p2[1]];
-}
-
-function mul(p, t) {
-    return [p[0] * t, p[1] * t];
+function collectResources(states) {
+    states.collectedOre += states.oreRobots;
+    states.collectedClay += states.clayRobots;
+    states.collectedObsidian += states.obsidianRobots;
+    states.collectedGeode += states.geodeRobots;
 }
